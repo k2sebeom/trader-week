060b42eb676339552cb32e602f98ccb8899a5d75
k2sebeom <moses97@gmail.com>
2024-08-20 14:22:51 +0900

    feat: Change lines between prompt

--- app/services/game_service.py
+++ app/services/game_service.py
@@ -39,7 +39,8 @@ EVENT_PROMPT = """We're playing stock price games with these companies.
 {companies}
 
 At each day, we get an event related to each company and stock price change corresponding to the event.
-Events should make a coherent story, and occasionally include super dramatic and fun price changes for certain companies.
+Events should make a coherent story as a whole.
+Occasionally include super dramatic and fun price changes for certain companies.
 
 All response in Korean. Give output in Json Format:"""
 
